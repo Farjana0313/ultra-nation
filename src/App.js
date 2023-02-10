196627b357960405fae4a757caf9449156ecace7
Farjana0313 <30821829+Farjana0313@users.noreply.github.com>
2023-02-10 09:16:40 +0600

    catch

--- src/App.js
+++ src/App.js
@@ -4,14 +4,15 @@ import { useEffect, useState } from 'react';
 
 function App() {
   const [countries, setCountries] = useState([]);
-  useEffect(()=>{
+  useEffect(() => {
     fetch('https://restcountries.com/v3.1/all')
-    .then(res=>res.json())
-    then(data=>setCountries(data))
-  },[]);
+      .then(res => res.json())
+      .then(data => setCountries(data))
+      .catch(error => console.log(error))
+  }, []);
   return (
     <div className="App">
-    <h1>Country loaded: {countries.length} </h1>
+      <h1>Country loaded: {countries.length} </h1>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
